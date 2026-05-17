2d5bdbf01be01f976c15f0fd8f115e25616f62a7
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-05-17 13:28:30 +0200

    Add files via upload
    Committing Main class.

---
+++ src/com/mystream/Main.java
@@ -0,0 +1,25 @@
+/**
+ *  Java program to create a Stream with 5 random double elements.
+ */
+package com.mystream;
+
+import java.util.Random;
+import java.util.stream.DoubleStream;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) {
+
+        // Creating a stream with doubles.
+        Random random = new Random();
+        DoubleStream myStream = random.doubles().limit(5);
+
+        // Printing myStream to console.
+        myStream.forEach(System.out::println);
+
+    }
+}
